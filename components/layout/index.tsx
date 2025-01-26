da0fe39ff53372d1ea370a044109ad4750bd97ca
Hung Nguyen <hung.nguyen.thanh@sotatek.com>
2025-01-26 11:10:47 +0700

    chore: set homepage

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
-      <div className="pt-[51px] max-tablet:pt-[82px]">{children}</div>
+      <div className="max-tablet:pt-[82px]">{children}</div>
     </div>
   );
 };
