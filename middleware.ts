be2fa19dd88033922941be97de627b02292f1919
Sotatek-QuanNguyen12 <quan.nguyen12@sotatek.com>
2025-02-07 11:34:45 +0700

    fix/login-agents

--- middleware.ts
+++ middleware.ts
@@ -17,7 +17,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/',
     '/((?!api/|_next/|static/|public/|google/callback|login|register|logo/|image|favicon.ico).*)',
   ],
 };
